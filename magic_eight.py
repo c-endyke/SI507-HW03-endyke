191426c0d4f7f0d0d17323c37b75931760d0a4aa
Samantha Brown <sammbrow@0587392202.wireless.umich.net>
2019-09-17 17:27:08 -0400

    final version?

--- magic_eight.py
+++ magic_eight.py
@@ -1,18 +1,22 @@
 
 def ask_question():
     question=input("What is your question?")
-    while not question.endswith("?") and not question == "quit":
-        print("I'm sorry, I can only answer questions")
-        question = input("What is your question?")
+    while not question.endswith("?"):
+        if question == "quit":
+            break
+        else:    
+            print("I'm sorry, I can only answer questions")
+            question = input("What is your question?")
     else:
+        give_answer()
         return question
-
-ask_question()
 
 import random
 
-lst_responses = ["It is certain.", "It is decidedly so.", "Without a doubt", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to Yes.", "Reply hazy, try again.", "Ask again later.", "Better not to tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
 
-choice = random.choice(lst_responses)
-
-print(choice)
+def give_answer():
+    lst_responses = ["It is certain.", "It is decidedly so.", "Without a doubt", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to Yes.", "Reply hazy, try again.", "Ask again later.", "Better not to tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
+    choice = random.choice(lst_responses)
+    print(choice)
+        
+ask_question()
