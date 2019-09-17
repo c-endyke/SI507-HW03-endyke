bdb18c999a47194c95ffb4d776351517986e7e69
Samantha Brown <sammbrow@0587392202.wireless.umich.net>
2019-09-17 17:03:29 -0400

    changes to make it ask question

--- magic_eight.py
+++ magic_eight.py
@@ -7,6 +7,8 @@ def ask_question():
     else:
         return question
 
+ask_question()
+
 import random
 
 lst_responses = ["It is certain.", "It is decidedly so.", "Without a doubt", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to Yes.", "Reply hazy, try again.", "Ask again later.", "Better not to tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
