fc2ab86d34e65417ee2ccc90db6310d3ad66dc2b
Samantha Brown <sammbrow@0587392202.wireless.umich.net>
2019-09-17 17:07:47 -0400

    trying again

--- magic_eight.py
+++ magic_eight.py
@@ -2,7 +2,7 @@
 def ask_question():
     question=input("What is your question?")
     while not question.endswith("?") and not question == "quit":
-        print("I’m sorry, I can only answer questions")
+        print("I'm sorry, I can only answer questions")
         question = input("What is your question?")
     else:
         return question
