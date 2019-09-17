7f5f6b7e4e172248c245aeb3aadd67e861d082f0
Samantha Brown <sammbrow@0587392202.wireless.umich.net>
2019-09-17 17:00:06 -0400

    taking out heads

--- magic_eight.py
+++ magic_eight.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 
 def ask_question():
     question=input("What is your question?")
@@ -8,12 +6,7 @@ def ask_question():
         question = input("What is your question?")
     else:
         return question
-=======
-<<<<<<< HEAD
-def ask_question(self):
-    question = input("What is your question?")
-    return question
-=======
+
 import random
 
 lst_responses = ["It is certain.", "It is decidedly so.", "Without a doubt", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to Yes.", "Reply hazy, try again.", "Ask again later.", "Better not to tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
@@ -21,5 +14,3 @@ lst_responses = ["It is certain.", "It is decidedly so.", "Without a doubt", "Ye
 choice = random.choice(lst_responses)
 
 print(choice)
->>>>>>> add_questions
->>>>>>> d70696d7dc0704752ca9524f192e04daa1e8060f
